78411a5a993fae068146d2a20afa927f66eeb917
Carolyn Whelpley <33427006+mcdonaldcarolyn@users.noreply.github.com>
2024-01-17 00:37:44 +0000

    create moves for players

--- main.py
+++ main.py
@@ -47,3 +47,11 @@ def isValidSpace(board, space):
 
 def isWinner(board, player):
     b, p = board, player
+    return (b['1'] == b['2'] == b['3'] == p) or
+            (b['4'] == b['5'] == b['6'] == p) or
+            (b['7'] == b['8'] == b['8'] == p) or
+            (b['1'] == b['4'] == b['7'] == p) or
+            (b['2'] == b['5'] == b['8'] == p) or
+            (b['3'] == b['6'] == b['9'] == p) or
+            (b['3'] == b['5'] == b['7'] == p) or
+            (b['1'] == b['5'] == b['9'] == p) 
