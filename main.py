f1550db1b46991f89ad634d355a1a9394c9354e8
Carolyn Whelpley <33427006+mcdonaldcarolyn@users.noreply.github.com>
2024-01-17 00:07:22 +0000

    create main variables and start game

---
+++ main.py
@@ -0,0 +1,16 @@
+ALL_SPACES = ['1', '2', '3', '4', '5','6', '7','8', '9']
+X, O, BLANK = 'X', 'O', ' '
+
+
+def main():
+    print('Welcome to Tic-Tac-Toe from invent with python')
+    gameBoard = getBlankBoard()
+    currentPlayer, nextPlayer = X, O
+
+    while True:
+        print(getBoardStr(gameBoard))
+        move = None
+        while not isValidSpace(gameBoard, move):
+            print('What is {}\'s move? (1-9)'.format(currentPlayer))
+            move = input('> ')
+        
